6757129dab411a53e33c0d0ab0c58e34e409cbec
Muneerah1612 <muniratolayiwola@gmail.com>
2022-05-10 10:18:20 +0100

    update-requirements-file

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginAPI, UserDetailAPI,RegisterUserAPIView
+from .views.auth_view import LoginAPI, UserDetailAPI,RegisterUserAPIView
 from knox import views as knox_views
 urlpatterns = [
   path("api/v1/get-details",UserDetailAPI.as_view()),
