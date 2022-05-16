7b4cff051de0f742698dc9c43af977a5035318fb
Jeremiah Jacob <jaykon4me@gmail.com>
2022-05-16 21:29:07 +0100

    redesign intro screen

---
+++ Frontend/lib/screens/home/multisig_setup.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MultisigSetup extends StatefulWidget {
+  const MultisigSetup({ Key? key }) : super(key: key);
+
+  @override
+  State<MultisigSetup> createState() => _MultisigSetupState();
+}
+
+class _MultisigSetupState extends State<MultisigSetup> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- Frontend/lib/screens/home/set_up.dart
+++ Frontend/lib/screens/home/set_up.dart
@@ -19,132 +19,146 @@ class _SetUpScreenState extends State<SetUpScreen> {
         child: Padding(
           padding: const EdgeInsets.only(top: 80, right: 20, left: 20),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Center(
-                child: Text(
-                  howItWorkText,
-                  style: AppTextStyle.textSize22.copyWith(
-                      color: AppColors.primaryColor,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
               Column(
                 children: [
-                  Height20(),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Column(
                     children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: const BoxDecoration(
-                          color: AppColors.skyBlueColor,
-                          shape: BoxShape.circle,
-                        ),
-                        child: Center(
-                          child: Text(
-                            '1',
-                            style: AppTextStyle.textSize18.copyWith(
-                                fontWeight: FontWeight.w600,
-                                color: AppColors.primaryColor),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        constraints: BoxConstraints(
-                            maxWidth: MediaQuery.of(context).size.width - 84),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 8, top: 5),
-                          child: Text(
-                            eachCosignerText,
-                            textAlign: TextAlign.justify,
-                            style: AppTextStyle.textSize18.copyWith(
-                                fontWeight: FontWeight.w400,
-                                color: AppColors.lightNeutral),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Height20(),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: const BoxDecoration(
-                          color: AppColors.skyBlueColor,
-                          shape: BoxShape.circle,
-                        ),
-                        child: Center(
-                          child: Text(
-                            '2',
-                            style: AppTextStyle.textSize18.copyWith(
-                                fontWeight: FontWeight.w600,
-                                color: AppColors.primaryColor),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8, top: 5),
+                      Center(
                         child: Text(
-                          allCosignerText,
-                          style: AppTextStyle.textSize18.copyWith(
-                              fontWeight: FontWeight.w400,
-                              color: AppColors.lightNeutral),
+                          howItWorkText,
+                          style: AppTextStyle.textSize22.copyWith(
+                              color: AppColors.primaryColor,
+                              fontWeight: FontWeight.w600),
                         ),
+                      ),
+                      Height20(),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            decoration: const BoxDecoration(
+                              color: AppColors.skyBlueColor,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Center(
+                              child: Text(
+                                '1',
+                                style: AppTextStyle.textSize18.copyWith(
+                                    fontWeight: FontWeight.w600,
+                                    color: AppColors.primaryColor),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            constraints: BoxConstraints(
+                                maxWidth:
+                                    MediaQuery.of(context).size.width - 84),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 8, top: 5),
+                              child: Text(
+                                eachCosignerText,
+                                textAlign: TextAlign.justify,
+                                style: AppTextStyle.textSize18.copyWith(
+                                    fontWeight: FontWeight.w400,
+                                    color: AppColors.lightNeutral),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
-              Column(
-                children: [
-                  Height20(),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Column(
                     children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: const BoxDecoration(
-                          color: AppColors.skyBlueColor,
-                          shape: BoxShape.circle,
-                        ),
-                        child: Center(
-                          child: Text(
-                            '3',
-                            style: AppTextStyle.textSize18.copyWith(
-                                fontWeight: FontWeight.w600,
-                                color: AppColors.primaryColor),
+                      Height40(),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            decoration: const BoxDecoration(
+                              color: AppColors.skyBlueColor,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Center(
+                              child: Text(
+                                '2',
+                                style: AppTextStyle.textSize18.copyWith(
+                                    fontWeight: FontWeight.w600,
+                                    color: AppColors.primaryColor),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      Container(
-                        constraints: BoxConstraints(
-                            maxWidth: MediaQuery.of(context).size.width - 84),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 8, top: 5),
-                          child: Text(
-                            withAll3Text,
-                            textAlign: TextAlign.justify,
-                            style: AppTextStyle.textSize18.copyWith(
-                                fontWeight: FontWeight.w400,
-                                color: AppColors.lightNeutral),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8, top: 5),
+                            child: Text(
+                              allCosignerText,
+                              style: AppTextStyle.textSize18.copyWith(
+                                  fontWeight: FontWeight.w400,
+                                  color: AppColors.lightNeutral),
+                            ),
                           ),
-                        ),
+                        ],
                       ),
                     ],
                   ),
+                  Column(
+                    children: [
+                      Height40(),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            decoration: const BoxDecoration(
+                              color: AppColors.skyBlueColor,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Center(
+                              child: Text(
+                                '3',
+                                style: AppTextStyle.textSize18.copyWith(
+                                    fontWeight: FontWeight.w600,
+                                    color: AppColors.primaryColor),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            constraints: BoxConstraints(
+                                maxWidth:
+                                    MediaQuery.of(context).size.width - 84),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 8, top: 5),
+                              child: Text(
+                                withAll3Text,
+                                textAlign: TextAlign.justify,
+                                style: AppTextStyle.textSize18.copyWith(
+                                    fontWeight: FontWeight.w400,
+                                    color: AppColors.lightNeutral),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Height50(),
                 ],
               ),
-              
+              CustomButton(
+                  onPressed: () {},
+                  text: Text('Continue',
+                      style: AppTextStyle.textSize21
+                          .copyWith(color: AppColors.whiteColor)),
+                  height: 60,
+                  width: 335),
             ],
           ),
         ),
