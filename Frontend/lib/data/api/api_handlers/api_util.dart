d0966be533d7da5d12f165374da9a0ca956b516c
Jeremiah Jacob <jaykon4me@gmail.com>
2022-05-19 11:13:43 +0100

    added api toast response

--- Frontend/lib/data/api/api_handlers/api_util.dart
+++ Frontend/lib/data/api/api_handlers/api_util.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:frontend/data/api/api.dart';
+import 'package:frontend/utils/utils.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ApiUtils {
   static ApiResponse toApiResponse(Response res) {
@@ -9,4 +12,14 @@ class ApiUtils {
       data: res.data,
     );
   }
+}
+showToastAnyWhere(String message, {ToastGravity gravity = ToastGravity.TOP}) {
+  Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: gravity,
+      timeInSecForIosWeb: 1,
+      backgroundColor: AppColors.primaryColor,
+      textColor: Colors.white,
+      fontSize: 16.0);
 }
