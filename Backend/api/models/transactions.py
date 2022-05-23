7516191b12b8f167fffa902ba2855b94b0ed7102
Muneerah1612 <muniratolayiwola@gmail.com>
2022-05-23 13:03:58 +0100

    ..

--- Backend/api/models/transactions.py
+++ Backend/api/models/transactions.py
@@ -9,19 +9,10 @@ class Transactions(models.Model):
     recipient_address=models.CharField(max_length=60)
     amount_sent=models.IntegerField()
     txn_fee=models.IntegerField()
-<<<<<<< HEAD
-    created_at=models.DateTimeField(auto_now_add=True)
-   
-    def __str__(self):
-        return self.tx_id
-
-
-=======
     transaction_id=models.CharField(max_length=255,blank=True, null=True)
     created_at=models.DateTimeField(auto_now_add=True)
    
     def __str__(self):
         return self.transaction_id
->>>>>>> 0a20c58ef5ad045a889ac625ce7445e97755b919
 
 
