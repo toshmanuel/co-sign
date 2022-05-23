14b97700d13e18200abe7e122ff5ba964e9e83a1
Muneerah1612 <muniratolayiwola@gmail.com>
2022-05-23 09:46:04 +0100

    ..

---
+++ Backend/api/models/transactions.py
@@ -0,0 +1,19 @@
+from django.db import models
+from .address import Addresses
+
+# Create your models here.
+class Transactions(models.Model):
+    tx_id =models.CharField(max_length=80)
+    address=models.ForeignKey(Addresses, on_delete=models.CASCADE)
+    txn_input=models.CharField(max_length=200)
+    recipient_address=models.CharField(max_length=60)
+    amount_sent=models.IntegerField()
+    txn_fee=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+   
+    def __str__(self):
+        return self.tx_id
+
+
+
+
