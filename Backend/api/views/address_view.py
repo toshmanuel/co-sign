474939cbbfe7280e96a74cac6d56e85acb86b93d
Olayiwola Olaoluwa <61384690+Muneerah1612@users.noreply.github.com>
2022-05-27 13:26:42 +0100

    Update address_view.py

--- Backend/api/views/address_view.py
+++ Backend/api/views/address_view.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from api.serializers.address import AddressSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
