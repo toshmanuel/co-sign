f2dea19b52b95a61ffece27b93f3b21be8a4a3a3
Jeremiah Jacob <jaykon4me@gmail.com>
2022-05-28 04:02:21 +0100

    remove unwanted screens and adjust home and receive screens

--- Frontend/lib/screens/home/home.dart
+++ Frontend/lib/screens/home/home.dart
@@ -1,7 +1,4 @@
-export 'generate_address_two.dart';
-export 'generate_address_one.dart';
 export 'home_page.dart';
 export 'home_screen.dart';
-export 'set_up.dart';
 export 'send_screen.dart';
 export 'receive_screen.dart';
--- Frontend/lib/screens/home/send_screen.dart
+++ Frontend/lib/screens/home/send_screen.dart
@@ -16,133 +16,134 @@ class _SendScreenState extends State<SendScreen> {
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController password2Controller = TextEditingController();
   TextEditingController privateKeyController = TextEditingController();
-  static List<String> friendsList = [];
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(right: 30, left: 30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  child: Container(
-                    height: 50,
-                    width: 50,
-                    decoration: const BoxDecoration(
-                      color: AppColors.skyBlueColor,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Center(
-                      child: SvgPicture.asset(
-                        AppVectors.arrowLeft,
-                        color: AppColors.primaryColor,
+      body: SafeArea(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 20, right: 30, left: 30, bottom: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: const BoxDecoration(
+                        color: AppColors.skyBlueColor,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Center(
+                        child: SvgPicture.asset(
+                          AppVectors.arrowLeft,
+                          color: AppColors.primaryColor,
+                        ),
                       ),
                     ),
+                    onTap: () {
+                      goBack(context);
+                    },
                   ),
-                  onTap: () {
-                    goBack(context);
-                  },
-                ),
-                Text(
-                  'Send Satoshis',
-                  style: AppTextStyle.textSize18.copyWith(
-                      color: AppColors.primaryColor,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
                   Text(
-                    'Create Transcation',
+                    'Send Satoshis',
                     style: AppTextStyle.textSize18.copyWith(
                         color: AppColors.primaryColor,
                         fontWeight: FontWeight.bold),
                   ),
-                  Height50(),
-                  Height50(),
-                  TextFormField(
-                    cursorColor: AppColors.primaryColor,
-                    autofocus: false,
-                    controller: emailController,
-                    decoration: AppDecorations.formStyle.copyWith(
-                      label: Text(
-                        'Address',
-                        style: AppTextStyle.textSize15
-                            .copyWith(color: AppColors.primaryColor),
-                      ),
-                    ),
-                  ),
-                  Height10(),
-                  TextFormField(
-                    cursorColor: AppColors.primaryColor,
-                    controller: passwordController,
-                    onSaved: (value) {
-                      passwordController.text = value!;
-                    },
-                    autofocus: false,
-                    textInputAction: TextInputAction.done,
-                    decoration: AppDecorations.formStyle.copyWith(
-                      label: Text(
-                        'Recipient',
-                        style: AppTextStyle.textSize15
-                            .copyWith(color: AppColors.primaryColor),
-                      ),
-                    ),
-                  ),
-                  Height10(),
-                  TextFormField(
-                    cursorColor: AppColors.primaryColor,
-                    controller: password2Controller,
-                    onSaved: (value) {
-                      passwordController.text = value!;
-                    },
-                    autofocus: false,
-                    textInputAction: TextInputAction.done,
-                    decoration: AppDecorations.formStyle.copyWith(
-                      label: Text(
-                        'Amount',
-                        style: AppTextStyle.textSize15
-                            .copyWith(color: AppColors.primaryColor),
-                      ),
-                    ),
-                  ),
-                  Height10(),
-                  TextFormField(
-                    cursorColor: AppColors.primaryColor,
-                    controller: password2Controller,
-                    onSaved: (value) {
-                      privateKeyController.text = value!;
-                    },
-                    autofocus: false,
-                    textInputAction: TextInputAction.done,
-                    decoration: AppDecorations.formStyle.copyWith(
-                      label: Text(
-                        'Private Key',
-                        style: AppTextStyle.textSize15
-                            .copyWith(color: AppColors.primaryColor),
-                      ),
-                    ),
-                  ),
                 ],
               ),
-            ),
-            CustomButton(
-              onPressed: () {},
-              text: Text('Create Transaction',
-                  style: AppTextStyle.textSize21
-                      .copyWith(color: AppColors.whiteColor)),
-              height: 60,
-              width: 335,
-            ),
-          ],
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Text(
+                      'Create Transcation',
+                      style: AppTextStyle.textSize18.copyWith(
+                          color: AppColors.primaryColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Height50(),
+                    TextFormField(
+                      cursorColor: AppColors.primaryColor,
+                      autofocus: false,
+                      controller: emailController,
+                      decoration: AppDecorations.formStyle.copyWith(
+                        label: Text(
+                          'Address',
+                          style: AppTextStyle.textSize15
+                              .copyWith(color: AppColors.primaryColor),
+                        ),
+                      ),
+                    ),
+                    Height10(),
+                    TextFormField(
+                      cursorColor: AppColors.primaryColor,
+                      controller: passwordController,
+                      onSaved: (value) {
+                        passwordController.text = value!;
+                      },
+                      autofocus: false,
+                      textInputAction: TextInputAction.done,
+                      decoration: AppDecorations.formStyle.copyWith(
+                        label: Text(
+                          'Recipient',
+                          style: AppTextStyle.textSize15
+                              .copyWith(color: AppColors.primaryColor),
+                        ),
+                      ),
+                    ),
+                    Height10(),
+                    TextFormField(
+                      cursorColor: AppColors.primaryColor,
+                      controller: password2Controller,
+                      onSaved: (value) {
+                        passwordController.text = value!;
+                      },
+                      autofocus: false,
+                      textInputAction: TextInputAction.done,
+                      decoration: AppDecorations.formStyle.copyWith(
+                        label: Text(
+                          'Amount',
+                          style: AppTextStyle.textSize15
+                              .copyWith(color: AppColors.primaryColor),
+                        ),
+                      ),
+                    ),
+                    Height10(),
+                    TextFormField(
+                      cursorColor: AppColors.primaryColor,
+                      controller: password2Controller,
+                      onSaved: (value) {
+                        privateKeyController.text = value!;
+                      },
+                      autofocus: false,
+                      textInputAction: TextInputAction.done,
+                      decoration: AppDecorations.formStyle.copyWith(
+                        label: Text(
+                          'Private Key',
+                          style: AppTextStyle.textSize15
+                              .copyWith(color: AppColors.primaryColor),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              CustomButton(
+                onPressed: () {},
+                text: Text('Create Transaction',
+                    style: AppTextStyle.textSize21
+                        .copyWith(color: AppColors.whiteColor)),
+                height: 60,
+                width: 335,
+              ),
+            ],
+          ),
         ),
       ),
     );
