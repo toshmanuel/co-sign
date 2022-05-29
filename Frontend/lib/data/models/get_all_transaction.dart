cea708592f3772db42a69594a70f782ff1324ee1
Jeremiah Jacob <jaykon4me@gmail.com>
2022-05-29 16:00:33 +0100

    updated the models with transactions models

---
+++ Frontend/lib/data/models/get_all_transaction.dart
@@ -0,0 +1,62 @@
+class GetAllTransactions {
+  int? status;
+  List<Transactions>? transactions;
+
+  GetAllTransactions({this.status, this.transactions});
+
+  GetAllTransactions.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    if (json['transactions'] != null) {
+      transactions = <Transactions>[];
+      json['transactions'].forEach((v) {
+        transactions!.add(new Transactions.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    if (this.transactions != null) {
+      data['transactions'] = this.transactions!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Transactions {
+  int? id;
+  String? recipient;
+  int? amount;
+  int? fee;
+  String? dateCreated;
+  bool? broadcasted;
+
+  Transactions(
+      {this.id,
+      this.recipient,
+      this.amount,
+      this.fee,
+      this.dateCreated,
+      this.broadcasted});
+
+  Transactions.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    recipient = json['recipient'];
+    amount = json['amount'];
+    fee = json['fee'];
+    dateCreated = json['date created'];
+    broadcasted = json['broadcasted'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['recipient'] = this.recipient;
+    data['amount'] = this.amount;
+    data['fee'] = this.fee;
+    data['date created'] = this.dateCreated;
+    data['broadcasted'] = this.broadcasted;
+    return data;
+  }
+}
